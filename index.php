3def34a2bfbb7f17d53ac9ef812caf8cd57bbd00
FatimaAide <hervertfatima47@gmail.com>
2024-06-19 08:45:58 -0600

    heienjwi

--- index.php
+++ index.php
@@ -10,10 +10,13 @@ echo "<br>";
 $x= 34;
 $y= 356.78;
 $z= $x+$y;
+$w= $x-$y;
+$f= $x*$y;
+$k= $x/$y;
 echo "La suma es $x + $y = $z <br>";
-echo "La resta es $x - $y = $z <br>";
-echo "La multiplicación es $x * $y = $z <br>";
-echo "La division es $x / $y = $z <br>";
+echo "La resta es $x - $y = $w <br>";
+echo "La multiplicación es $x * $y = $f <br>";
+echo "La division es $x / $y = $k <br>";
 ?>
 
 
