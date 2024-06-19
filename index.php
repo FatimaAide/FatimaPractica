9e61f792e805e1612cc854338fe2f16f7568451f
FatimaAide <hervertfatima47@gmail.com>
2024-06-19 08:33:35 -0600

    argeb

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+echo "My first PHP script!";
+echo "<br>";
+echo "Mis datos:";
+$nombre ="Fatima Aide Hervert Espinoza";
+$edad = 18;
+$estatura = 1.58;
+echo "Soy ..." . $nombre . " tengo " . $edad . " años y mido " .$estatura . "metros" . $estatura . "metros";
+echo "<br>";
+$x= 34;
+$y= 356.78;
+$z= $x+$y;
+echo "La suma es $x + $y = $z <br>";
+echo "La resta es $x - $y = $z <br>";
+echo "La multiplicación es $x * $y = $z <br>";
+echo "La division es $x / $y = $z <br>";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practicas PHP</title>
+</head>
+<body>
+    
+</body>
+</html>
